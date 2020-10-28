dd566637547f62b074eb2986c2d0d5ae3364998d
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-10-28 12:01:47 +0200

    Updated project
    add bd models to mvc project

---
+++ Dimension_Data_Demo/Dimension_Data_Demo/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dimension_Data_Demo.Models
+{
+    public partial class Employee
+    {
+        public int EmployeeNumber { get; set; }
+        public int? JobId { get; set; }
+        public int? DetailsId { get; set; }
+        public int? PayId { get; set; }
+        public int? EducationId { get; set; }
+        public int? SurveyId { get; set; }
+        public int? HistoryId { get; set; }
+        public int? PerformanceId { get; set; }
+
+        public virtual EmployeeDetails Details { get; set; }
+        public virtual EmployeeEducation Education { get; set; }
+        public virtual EmployeeHistory History { get; set; }
+        public virtual JobInformation Job { get; set; }
+        public virtual CostToCompany Pay { get; set; }
+        public virtual EmployeePerformance Performance { get; set; }
+        public virtual Surveys Survey { get; set; }
+    }
+}
---
+++ Dimension_Data_Demo/Dimension_Data_Demo/Models/JobInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dimension_Data_Demo.Models
+{
+    public partial class JobInformation
+    {
+        public JobInformation()
+        {
+            Employee = new HashSet<Employee>();
+        }
+
+        public int JobId { get; set; }
+        public string JobRole { get; set; }
+        public string Department { get; set; }
+        public int? JobLevel { get; set; }
+        public int? StandardHours { get; set; }
+        public int? EmployeeCount { get; set; }
+        public string BusinessTravel { get; set; }
+        public int? StockOptionLevel { get; set; }
+
+        public virtual ICollection<Employee> Employee { get; set; }
+    }
+}
